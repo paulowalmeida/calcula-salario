118a950f932e127378b6d854e5eb6d12f52b1780
Paulo W. A. Ferreira <pauloweskley.dev@gmail.com>
2020-09-02 17:34:33 -0300

    - Melhor organização dos componentes; - Transformação de componentes e functions;

---
+++ build/precache-manifest.c72e5840c153e17b3352b1e6577b9b89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504f5413919fc4cd5762d8117bfb0d2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb70954365d8163f1339",
+    "url": "/static/css/main.738c66ea.chunk.css"
+  },
+  {
+    "revision": "2eb44ab9ba1a411d1383",
+    "url": "/static/js/2.644d33b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.644d33b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb70954365d8163f1339",
+    "url": "/static/js/main.f8f8171c.chunk.js"
+  },
+  {
+    "revision": "e654c26b71d69da42391",
+    "url": "/static/js/runtime-main.c24bcf23.js"
+  },
+  {
+    "revision": "33d69fdc37adb36ab83c51ecad548ed5",
+    "url": "/static/media/background.33d69fdc.jpg"
+  }
+]);
